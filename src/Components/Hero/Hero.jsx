bb65535b8be2e9a6ec39f39ab8e9dbb8eeb1718d
Isi Name <perpetualokan5@gmail.com>
2025-02-28 18:42:30 +0000

    edited her.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,13 +7,9 @@ const Hero = () => {
     <div id='home' className='hero'>
       <img src={profile} alt="" />
     <h1><span>I'm Perpetual Okan,</span>frontend developer based in Nigeria</h1>
-    <p>I am a skilled frontend developer from Edo, Nigeria,
-       with two years of experience in building responsive
-        and user-friendly web applications. I specialize in creating modern,
-         interactive, and visually appealing websites using HTML, CSS, and JavaScript, 
-         along with frameworks like React. Passionate about clean code and great
-          user experiences, I continuously enhance my skills to stay updated with
-           the latest web development trends and technologies.
+    <p>I am a frontend developer from Edo, Nigeria, 
+      with two years of experience in building responsive and interactive web applications
+       using HTML, CSS, JavaScript, and React.
 
 </p>
     <div className="hero-action">
